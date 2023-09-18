0a4535506a3cd9df39530a31372f3ecd299a83f4
viet <hoquocviet.na.dio@gmail.com>
2023-09-18 10:18:07 +0700

    fetch token using useEffect

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
-  const reqBody = {
-    username: "hoviet",
-    password: "123456",
-  };
-  fetch("api/auth/login", {
-    headers: {
-      "Content-Type": "application/json",
-    },
-    method: "post",
-    body: JSON.stringify(reqBody),
-  })
-    .then((response) => Promise.all([response.json(), response.headers]))
-    .then(([body, headers]) => {
-      console.log(headers.get("authorization"));
-      console.log(body);
-    });
-  return <div className="App">Hello</div>;
+  const [jwt, setJwt] = useState("");
+
+  useEffect(() => {
+    const reqBody = {
+      username: "hoviet",
+      password: "123456",
+    };
+    fetch("api/auth/login", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "post",
+      body: JSON.stringify(reqBody),
+    })
+      .then((response) => Promise.all([response.json(), response.headers]))
+      .then(([body, headers]) => {
+        setJwt(headers.get("authorization"));
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(`Jwt is: ${jwt}`);
+  }, [jwt]);
+
+  return (
+    <div className="App">
+      <div>{jwt}</div>
+    </div>
+  );
 }
 
 export default App;
