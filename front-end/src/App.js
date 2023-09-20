2a7f8517c174832d8b774a804e09f7c8b3989790
viet <hoquocviet.na.dio@gmail.com>
2023-09-20 11:01:33 +0700

    connect login page with api

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,6 +1,4 @@
-import { useEffect } from "react";
 import "./App.scss";
-import { useLocalState } from "./store/UseLocalStorage";
 import { Route, Routes } from "react-router-dom";
 // component import
 import DashBoard from "./components/DashBoard/index.jsx";
@@ -9,32 +7,6 @@ import Login from "./components/Login";
 import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
-  const [jwt, setJwt] = useLocalState("", "jwt");
-
-  // useEffect(() => {
-  //   if (!jwt) {
-  //     const reqBody = {
-  //       username: "hoviet",
-  //       password: "123456",
-  //     };
-  //     fetch("api/auth/login", {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       method: "post",
-  //       body: JSON.stringify(reqBody),
-  //     })
-  //       .then((response) => Promise.all([response.json(), response.headers]))
-  //       .then(([body, headers]) => {
-  //         setJwt(headers.get("authorization"));
-  //       });
-  //   }
-  // }, [jwt, setJwt]);
-
-  // useEffect(() => {
-  //   console.log(`Jwt is: '${jwt}'`);
-  // }, [jwt]);
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
--- front-end/src/components/Login/index.jsx
+++ front-end/src/components/Login/index.jsx
@@ -1,16 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocalState } from "src/store/UseLocalStorage";
+// library
+import {
+  NotificationContainer,
+  NotificationManager,
+} from "react-notifications";
+// css
+import "react-notifications/lib/notifications.css";
 
 const Login = () => {
+  const [jwt, setJwt] = useLocalState("", "jwt");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const sendLoginRequest = (type) => {
+    if (!jwt) {
+      const reqBody = {
+        username: username,
+        password: password,
+      };
+      fetch("api/auth/login", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "post",
+        body: JSON.stringify(reqBody),
+      })
+        .then((response) => {
+          if (response.status === 200) {
+            return Promise.all([response.json(), response.headers]);
+          } else {
+            return Promise.reject("Invalid login attempt!");
+          }
+        })
+        .then(([body, headers]) => {
+          setJwt(headers.get("authorization"));
+          window.location.href = "dashboard";
+        })
+        .catch((message) => {
+          NotificationManager.warning(message, "Warning", 2000);
+          // alert(message);
+        });
+    }
+  };
+
   return (
     <div>
       <div>
         <label htmlFor="username">username</label>
-        <input type="email" id="username" />
+        <input
+          type="email"
+          id="username"
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
       </div>
       <div>
         <label htmlFor="password">password</label>
-        <input type="password" id="password" />
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
       </div>
+      <div>
+        <button id="submit" type="submit" onClick={sendLoginRequest}>
+          Submit
+        </button>
+      </div>
+      <NotificationContainer />
     </div>
   );
 };
